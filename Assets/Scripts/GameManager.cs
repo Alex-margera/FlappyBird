30a2a48f8f3677ed927de149981360e1f807c8a5
Александр Тарасенко <aleksandrtarasenko@MacBook-Air-Aleksandr.local>
2019-08-26 01:38:05 +0300

    Tubes Generation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-   
-    void Start()
+    private GameObject tubes;
+    public float spawnTime;
+    public GameObject RootTubes;
+
+    void Awake()
     {
+        
         
     }
 
-   
+    void Start()
+
+    
+    {
+        StartCoroutine(GenerateTubes());   
+    }
+
+    IEnumerator GenerateTubes()
+    {
+        Vector2 position;
+
+        while (true)
+        {
+            position = transform.position;
+            position.x += 6f;
+            Instantiate(RootTubes, position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+
+
     void Update()
     {
         
     }
+
+
 }
